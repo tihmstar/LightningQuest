7893828100fab237ab7e33138df0f35c9ef71ec8
Malte Poll <malte.poll@web.de>
2020-07-21 20:55:50 +0200

    Added command handler dependency

--- src/main/java/net/tihmstar/LightningQuest/LightningQuestMod.java
+++ src/main/java/net/tihmstar/LightningQuest/LightningQuestMod.java
@@ -16,7 +16,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mojang.brigadier.*;
+
 import java.util.stream.Collectors;
+
+import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
+import static com.mojang.brigadier.builder.RequiredArgumentBuilder.argument;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("lightningquest")
@@ -44,6 +51,23 @@ public class LightningQuestMod
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
+        CommandDispatcher<Object> dispatcher = new CommandDispatcher<>();
+
+        dispatcher.register(
+                literal("foo")
+                        .then(
+                                argument("bar", integer())
+                                        .executes(c -> {
+                                            System.out.println("Bar is " + getInteger(c, "bar"));
+                                            return 1;
+                                        })
+                        )
+                        .executes(c -> {
+                            System.out.println("Called foo with no arguments");
+                            return 1;
+                        })
+        );
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
