c229102c6fecb23af0a85f79131e73d8ebe380fa
Malte Poll <malte.poll@web.de>
2020-07-21 20:10:42 +0200

    renamed even more stuff

--- src/main/java/net/tihmstar/LightningQuest/LightningQuestMod.java
+++ src/main/java/net/tihmstar/LightningQuest/LightningQuestMod.java
@@ -54,7 +54,7 @@ public class LightningQuestMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("lightningquest", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
