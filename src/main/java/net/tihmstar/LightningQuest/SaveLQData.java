b7046cb46de38a471f3ccfecce22f37a10ff90ed
Malte Poll <malte.poll@web.de>
2020-07-22 18:58:44 +0200

    started implementing world saved data for squad memberships

--- src/main/java/net/tihmstar/LightningQuest/SaveLQData.java
+++ src/main/java/net/tihmstar/LightningQuest/SaveLQData.java
@@ -3,9 +3,15 @@ package net.tihmstar.LightningQuest;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.world.storage.WorldSavedData;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class SaveLQData extends WorldSavedData {
     private static final String MODID = "lightningquest";
     private static final String DATA_NAME = MODID + "_SquadMemberships";
+    private static final String SQUADS_KEY  = DATA_NAME + "_squads" ;
+
+    private String jsonData;
 
     // Required constructors
     public SaveLQData() {
@@ -17,12 +23,20 @@ public class SaveLQData extends WorldSavedData {
 
     @Override
     public void read(CompoundNBT nbt) {
-
+        // TODO: deserialize JSON data
+        jsonData = nbt.getString(SQUADS_KEY);
     }
     
     @Override
     public CompoundNBT write(CompoundNBT nbt) {
+        nbt.putString(SQUADS_KEY, jsonData);
         return nbt;
+    }
+
+    public void updateSquadMemberships(HashMap<UUID, Squad> uuidSquadHashMap) {
+        //jsonData =
+        // TODO: serialize JSON data
+        markDirty();
     }
 
     // WorldSavedData methods
